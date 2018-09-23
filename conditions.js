1238370b7d0ab6a7ff8e0c3ccba0506e92fab044
Kamil Olber <olb.kamil@gmail.com>
2018-09-23 10:28:34 +0200

    switch/ case/ default

--- conditions.js
+++ conditions.js
@@ -1,28 +1,24 @@
 
-/*const truthy = true;
-const a = 1;
 
-if (truthy)
-    console.log('It works');
-
-if (true || false){
-    const a =2;
-
-    console.log("What's up?");
-
-}
-
-if (true) console.log(1);
-if(false) console.log("false");
-if(true) console.log(2);
-*/
-
-const lol = "hello";
-
-if(lol === 1) {
+const numb = "1";
+/*
+if (numb === 1) {
     console.log("It's number 1!");
-} else if(lol === "1") {
+} else if (numb === "1") {
     console.log("It's string 1!");
 } else {
     console.log("Don't know what's going on...");
+}
+
+*/
+
+switch (numb) {
+    case 1:
+        console.log("It's number 1?!");
+        break;
+    case "1":
+        console.log("It's string 1?!");
+        break;
+    default:
+        console.log("lolloon...");
 }
