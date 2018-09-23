b1c9c9b00172e6d230f97e0583c09735793c8e0c
Kamil Olber <olb.kamil@gmail.com>
2018-09-23 10:13:40 +0200

    if number/ string/ else

---
+++ conditions.js
@@ -0,0 +1,28 @@
+
+/*const truthy = true;
+const a = 1;
+
+if (truthy)
+    console.log('It works');
+
+if (true || false){
+    const a =2;
+
+    console.log("What's up?");
+
+}
+
+if (true) console.log(1);
+if(false) console.log("false");
+if(true) console.log(2);
+*/
+
+const lol = "hello";
+
+if(lol === 1) {
+    console.log("It's number 1!");
+} else if(lol === "1") {
+    console.log("It's string 1!");
+} else {
+    console.log("Don't know what's going on...");
+}
--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var foo = 0;
-var boo = foo || "hello";
-console.log("boo :",boo);
-console.log("foo:", foo);
+var boo = foo || "hello"; // hello
+
+var baz = 1;
+var zoo = baz || "hey!"; //1
+
+var foz = false;
+var boz = !foz || 0 || 42; //true
+
+
+
+
+
+console.log("boo: ",boo);
+console.log("foo: ",foo);
+console.log("boz: ",boz);
 
